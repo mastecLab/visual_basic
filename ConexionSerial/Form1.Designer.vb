b0fcd2c5865e501630c63f25ae186caba879fcb7
mastec <sebagtz@gmail.com>
2023-10-15 20:14:56 +0200

    Add files via upload

---
+++ ConexionSerial/Form1.Designer.vb
@@ -0,0 +1,146 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.btnDeterminarConexion = New System.Windows.Forms.Button()
+        Me.cboPuertos = New System.Windows.Forms.ComboBox()
+        Me.btnConectar = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.SerialPort1 = New System.IO.Ports.SerialPort(Me.components)
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.spPuertos = New System.IO.Ports.SerialPort(Me.components)
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnDeterminarConexion
+        '
+        Me.btnDeterminarConexion.BackColor = System.Drawing.SystemColors.HotTrack
+        Me.btnDeterminarConexion.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.btnDeterminarConexion.Cursor = System.Windows.Forms.Cursors.Default
+        Me.btnDeterminarConexion.Font = New System.Drawing.Font("MS Reference Sans Serif", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnDeterminarConexion.Location = New System.Drawing.Point(41, 13)
+        Me.btnDeterminarConexion.Margin = New System.Windows.Forms.Padding(4)
+        Me.btnDeterminarConexion.Name = "btnDeterminarConexion"
+        Me.btnDeterminarConexion.Size = New System.Drawing.Size(172, 36)
+        Me.btnDeterminarConexion.TabIndex = 0
+        Me.btnDeterminarConexion.Text = "Puerto"
+        Me.btnDeterminarConexion.UseVisualStyleBackColor = False
+        '
+        'cboPuertos
+        '
+        Me.cboPuertos.FormattingEnabled = True
+        Me.cboPuertos.Location = New System.Drawing.Point(239, 13)
+        Me.cboPuertos.Margin = New System.Windows.Forms.Padding(4)
+        Me.cboPuertos.Name = "cboPuertos"
+        Me.cboPuertos.Size = New System.Drawing.Size(147, 24)
+        Me.cboPuertos.TabIndex = 1
+        '
+        'btnConectar
+        '
+        Me.btnConectar.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.btnConectar.Font = New System.Drawing.Font("MS Reference Sans Serif", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnConectar.Location = New System.Drawing.Point(41, 57)
+        Me.btnConectar.Margin = New System.Windows.Forms.Padding(4)
+        Me.btnConectar.Name = "btnConectar"
+        Me.btnConectar.Size = New System.Drawing.Size(172, 36)
+        Me.btnConectar.TabIndex = 2
+        Me.btnConectar.Text = "Conectar"
+        Me.btnConectar.UseVisualStyleBackColor = False
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(239, 57)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(147, 22)
+        Me.TextBox1.TabIndex = 4
+        '
+        'Timer1
+        '
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Font = New System.Drawing.Font("Myriad Web Pro", 49.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(39, 256)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(67, 95)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "-"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.Color.Transparent
+        Me.Label2.Font = New System.Drawing.Font("Myriad Web Pro", 49.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(163, 257)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(121, 95)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "°C"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.BackColor = System.Drawing.Color.Transparent
+        Me.Label3.Font = New System.Drawing.Font("Adobe Gothic Std B", 49.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(23, 162)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(546, 104)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Temperature"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(984, 482)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.btnConectar)
+        Me.Controls.Add(Me.cboPuertos)
+        Me.Controls.Add(Me.btnDeterminarConexion)
+        Me.Margin = New System.Windows.Forms.Padding(4)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.TransparencyKey = System.Drawing.Color.Maroon
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnDeterminarConexion As System.Windows.Forms.Button
+    Friend WithEvents cboPuertos As System.Windows.Forms.ComboBox
+    Friend WithEvents btnConectar As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents SerialPort1 As System.IO.Ports.SerialPort
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents spPuertos As System.IO.Ports.SerialPort
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+End Class
---
+++ ConexionSerial/Form1.vb
@@ -0,0 +1,84 @@
+﻿
+Public Class Form1
+    Dim strBufferOut As String
+    Dim strBufferIn As String
+    Dim iBufferIn1 As Integer
+    Dim resultado As Double
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        btnConectar.Enabled = False
+        Timer1.Enabled = False
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        If spPuertos.BytesToRead <> 0 Then
+            iBufferIn1 = spPuertos.ReadExisting
+            spPuertos.DiscardInBuffer()
+            resultado = iBufferIn1
+            TextBox1.Text = resultado
+            Label1.Text = TextBox1.Text
+
+    End Sub
+
+    Private Sub btnConectar_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnConectar.Click
+        If btnConectar.Text = ("Conectar") Then
+            spPuertos.PortName = cboPuertos.Text
+            btnConectar.Text = ("Desconectar")
+            Timer1.Start()
+            spPuertos.Open()
+
+
+        ElseIf btnConectar.Text = ("Desconectar") Then
+            btnConectar.Text = ("Conectar")
+            Timer1.Stop()
+            spPuertos.Close()
+
+        End If
+    End Sub
+
+    Private Sub btnDeterminarConexion_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnDeterminarConexion.Click
+        cboPuertos.Items.Clear()
+
+        For Each PuertosDisponibles As String In My.Computer.Ports.SerialPortNames
+            cboPuertos.Items.Add(PuertosDisponibles)
+        Next
+
+        If cboPuertos.Items.Count > 0 Then
+            cboPuertos.Text = cboPuertos.Items(0)
+            ' btnConectar.Enabled = True
+
+            spPuertos.PortName = cboPuertos.Text
+            btnConectar.Text = ("Desconectar")
+            spPuertos.Open()
+            Timer1.Start()
+
+
+        Else
+            MessageBox.Show("¡Ningun Puerto Encontrado!")
+            btnConectar.Enabled = False
+            cboPuertos.Items.Clear()
+            cboPuertos.Text = ("                          ")
+
+        End If
+    End Sub
+
+
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+        Dim Intru As Object
+        Dim Archivo As Object
+        Intru = CreateObject("Scripting.FileSystemObject")
+        Archivo = Intru.CreateTextFile("D:\flujo\flujo.txt", True)
+        Archivo.WriteLine("flujo=")
+        Archivo.WriteLine(resultado)
+        Archivo.Close()
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+End Class
+
+
+
